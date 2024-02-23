d9f9cb54b41352e6448ab118f3d0893ca7dac1dc
Ekechukwu David <63519479+davideke1@users.noreply.github.com>
2024-02-23 11:02:00 +0530

    Update urls.py
    Removed gusto2024

--- gusto2024/urls.py
+++ gusto2024/urls.py
@@ -23,7 +23,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gusto2024/',include('main.urls')),
+    path('',include('main.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL,
