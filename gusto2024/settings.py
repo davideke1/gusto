253cff2e876abe7185c20d5d0b3691fff5e9f0fa
David <ekechukwudavid@gmail.com>
2024-02-23 18:16:42 +0530

    debug condiditon changed

--- gusto2024/settings.py
+++ gusto2024/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECRET_KEY = os.getenv('SECRET_KEY')
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
-DEBUG =False
+DEBUG =True
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh' , '*']
 
 
